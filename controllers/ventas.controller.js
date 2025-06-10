47ebc3daa5b12ddd460ac969061a4e41cef26e86
Cristian <cristian.e.druetta@gmail.com>
2025-06-09 23:00:29 -0300

    cruds

--- controllers/ventas.controller.js
+++ controllers/ventas.controller.js
@@ -14,6 +14,42 @@ const getAllSells = async (req, res) => {
 
     } 
 }
+
+const getSellById = async (req, res) => {
+
+    try{
+        const venta = await Venta.findByPk(req.params.id, {
+            include: [Usuario, Producto],
+        });
+
+        if (!venta) {
+            return res.status(404).json({message: 'Venta no encontrada'});
+        }
+        res.json({status: 200, data: venta, message: 'Venta obtenida exitosamente'});
+    } catch (error) {
+        res.status(500).json({status: 500, message: 'Error al obtener las ventas', error: error});
+
+    }
+    
+    
+    const createSell = async (req, res) => {
+        const { ûsuarioId, productoId, cantidad, total, fecha } = req.body;
+        try{
+            if (!usuarioId || !productoId || !cantidad || !total || !fecha) {
+                return res.status(400).json({message: 'Faltan campos obligatorios'});
+            }
+            const newSale = await Venta.create({usuarioId, productoId, cantidad, total, fecha});
+            res.json({status: 201, data: newSale, message: 'Venta creada exitosamente'});      
+        } catch (error) {
+            res.status(500).json({status: 500, message: 'Error al crear la venta', error: error});
+        }
+    };
+
+    
     module.exports = {
-        getAllSells
+        getAllSells,
+        getSellByI,
+        createSell,
+    };
+    
 };
